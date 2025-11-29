34b2b0595f781b60cc9e2efe8e84bb12754ff67f
Roco-LEE <aowlrtmd@gmail.com>
2025-11-29 21:25:02 +0900

    251129

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('남자 코트 추천'); // 데이터 변경 시 html 재 렌더링 for 웹앱
+
+  let posts = '강남 고기 맛집'; // 데이터 변경 시 새로고침 됨
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          하이요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <h4> { posts } </h4>
+      <div className='list'>
+        <h3>{ posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목 }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
