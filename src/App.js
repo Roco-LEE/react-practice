1b5b49bfc08d5f74b9a4ab996fa310c8711ff131
Roco-LEE <aowlrtmd@gmail.com>
2025-12-01 00:01:25 +0900

    251201

--- src/App.js
+++ src/App.js
@@ -4,24 +4,35 @@ import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState('남자 코트 추천'); // 데이터 변경 시 html 재 렌더링 for 웹앱
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '오사카 여행후기']); // 데이터 변경 시 html 재 렌더링 for 웹앱
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집'; // 데이터 변경 시 새로고침 됨
+
+  function 제목바꾸기() {
+    var newArray = [...글제목]; // deep copy
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <h4> { posts } </h4>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className='list'>
-        <h3>{ posts }</h3>
+        <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className='list'>
-        <h3>{ 글제목 }</h3>
-        <p>2월 17일 발행</p>
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
     </div>
